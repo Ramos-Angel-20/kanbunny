1689cb9c59c04fd48b46df55dbbe26d1dca30b2e
Ramos-Angel-20 <miguelan47@yahoo.com.mx>
2021-12-04 00:23:03 -0800

    Se mejoro el componente column

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { Flex } from '@chakra-ui/layout';
 
 import AuthPage from './pages/AuthPage';
 import ProjectsPage from './pages/ProjectsPage';
-import Column from './components/Column';
 
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path='/account' element={<AuthPage/>}/>
-    //   <Route path='/projects' element={<ProjectsPage/>}/>
-    // </Routes>
-    <Flex backgroundColor='#cecece' h='100vh' w='full' align='spa' justify='center' overflow='hidden'>
-      <Column/>
-    </Flex>
+    <Routes>
+      <Route path='/account' element={<AuthPage/>}/>
+      <Route path='/projects' element={<ProjectsPage/>}/>
+    </Routes>
   );
 }
 
--- src/components/ProjectsSidebar.js
+++ src/components/ProjectsSidebar.js
@@ -4,15 +4,15 @@ import { IoIosJournal } from 'react-icons/io';
 
 const ProjectsSidebar = () => {
     
-    const containerBgColor = useColorModeValue('gray.50', 'gray.700');
+    const containerBgColor = useColorModeValue('gray.100', 'gray.700');
 
     return (
         <VStack flexShrink={0} paddingY={4} width='full' height='full' maxW={{ base: 56, '2xl': 72 }} justify='flex-start' maxH='full' >
-            <HStack w='full' borderRadius='md' flex={1} border='1px solid lightgray' maxH='50px' p={2} backgroundColor={containerBgColor} cursor='pointer'>
+            <HStack w='full' borderRadius='md' flex={1} maxH='50px' p={2} backgroundColor={containerBgColor} cursor='pointer'>
                 <HiViewBoards/>
                 <Text fontWeight='600'>Kanban Boards</Text>
             </HStack>
-            <HStack w='full' borderRadius='md' flex={1} border='1px solid lightgray' maxH='50px' p={2} backgroundColor={containerBgColor} cursor='pointer'>
+            <HStack w='full' borderRadius='md' flex={1} maxH='50px' p={2} backgroundColor={containerBgColor} cursor='pointer'>
                 <IoIosJournal/>
                 <Text fontWeight='600'>Journals</Text>
             </HStack>
